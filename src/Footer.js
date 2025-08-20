97e0a8b1f672ff2f1081c8fd069e07a5f60efd9b
Hemant Sharma <pvttpndt2gmail.com>
2025-08-20 14:27:28 +0530

    done

--- src/Footer.js
+++ src/Footer.js
@@ -6,7 +6,7 @@ function Footer() {
       <div className="space-x-4 text-sm font-medium flex justify-center gap-4 flex-wrap">
        
        <p className="text-center text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-indigo-600 to-purple-700 font-semibold text-lg">
-  Todo React App Created By Hemant | IT 1st Year | Roll No 28241233
+  Todo React App Created By Hemant | IT 2nd Year | Roll No 28241233
 </p>
       </div>
     </footer>
